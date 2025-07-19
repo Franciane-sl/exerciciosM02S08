80f12f2730a3e30ad60d9de0de20d65f8da3ab16
Franciane SL <fran.schier@outlook.com>
2025-07-19 18:10:10 -0300

    Resolução do exercicio1.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <header className='headerApp'>
+      <h1>Energia Solar 365</h1>
+      <p>Economize com o sol. Invista no seu futuro!</p>
+    </header>
+  )
+}
+
+export default App
