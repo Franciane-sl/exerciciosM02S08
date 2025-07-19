f746b880610d4f9308402df8e6b20bffe40005cd
Franciane SL <fran.schier@outlook.com>
2025-07-19 20:43:33 -0300

    Implementação do component navbar no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
-
+import Navbar from './components/Navbar'
 function App() {
   
 
   return (
+    <>
     <header className='headerApp'>
       <h1>Energia Solar 365</h1>
       <p>Economize com o sol. Invista no seu futuro!</p>
     </header>
+     <Navbar />
+    
+    </>
   )
 }
 
