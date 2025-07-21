8ddb2f42fb9439273c79b9210d384543a8d09328
Franciane SL <fran.schier@outlook.com>
2025-07-20 21:48:02 -0300

    Implementação do componente porque investir.

---
+++ src/components/PorQueInvestir/PorQueInvestir.jsx
@@ -0,0 +1,23 @@
+import './PorQueInvestir.css'
+
+function PorQueInvestir(){
+    return(
+        <>
+        <section className='pqinvestir' id='porqueInvestir'>
+            <h2>Por que investir em Energia Solar?</h2>
+            <ul>
+                <li><strong>Redução Imediata na Conta de Energia</strong></li>
+                <p>Com a instalação de sistemas fotovoltaicos eficientes, nossos clientes economizam até 95% na fatura de energia elétrica desde o primeiro mês</p>
+                <li><strong>Tecnologia de Ponta com Garantia e Suporte</strong></li>
+                <p>Trabalhamos apenas com equipamentos certificados e de alta performance, oferecendo garantia estendida, suporte técnico especializado e manutenção programada.</p>
+                <li><strong>Valorização do Imóvel e Retorno Garantido</strong></li>
+                <p>Investir em energia solar aumenta o valor de mercado do seu imóvel e proporciona retorno financeiro médio entre 3 a 5 anos, com vida útil superior a 25 anos.</p>
+                <li><strong>Compromisso com o Meio Ambiente e Sustentabilidade</strong></li>
+                <p>Ao produzir sua própria energia limpa, você contribui para a preservação do meio ambiente e a redução das emissões de CO₂, alinhando sua escolha a práticas ESG.</p>
+            </ul>
+        </section>
+        </>
+    )
+}
+
+export default PorQueInvestir;
